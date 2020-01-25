885d378bab56054468641db6e4ef2673b602ff0a
Jose Sanchez <jose@serhost.com>
2020-01-25 17:40:40 +0100

    Actualizacion BD para futuro

---
+++ ambrosio_v0.3.sql
@@ -0,0 +1,19 @@
+alter table incidencia add mac varchar(15) default null;
+
+CREATE TABLE ordenador(
+	idordenador integer PRIMARY KEY auto_increment,
+	idlocalizacion integer references localizacion(idlocalizacion),
+	etiqueta varchar(30) not null,
+	mac varchar(15) not null,
+	dns varchar(250) null,
+	ip varchar(15) null,
+	procesador varchar(75),
+	ssd varchar(25),
+	hdd varchar(25),
+	ram varchar(25),
+	fila integer default 0,
+	columna integer default 0,
+	fechaInstalacion timestamp default now(),
+	fechaMontaje timestamp default now()
+);
+
