05decdbd9e3cc54a7f3bda97335db1d94e29b99d
Jose Sanchez <jose@serhost.com>
2020-02-16 19:38:13 +0100

    Arreglo log-out para sesion real

--- includes/logout.php
+++ includes/logout.php
@@ -3,6 +3,7 @@ $_SESSION['usuario']="";
 $_SESSION['idusuario']="";
 $_SESSION['grupos']="";
 $_SESSION['esadmin']="";
+$_SESSION['sesionreal']=false;
 session_destroy();
 session_abort();
 header("Location: ?op=crearincidencia");
